17aabde352d4453c14094382e38e14d7ce84d7be
Thomas Stjernegaard <tsjeppesen@gbif.org>
2019-01-10 13:27:52 +0100

    More space in detail row

--- src/pages/NameSearch/RowDetail.js
+++ src/pages/NameSearch/RowDetail.js
@@ -5,18 +5,18 @@ import _ from 'lodash'
 export default   ({issues, usage, classification}) => (
     <React.Fragment>
         {_.get(usage, 'id') &&   <Row style={{marginBottom: '10px'}}>
-            <Col span={2} style={{textAlign: 'right', paddingRight: '16px', fontWeight: 'bold'}}>ID:</Col>
+            <Col span={3} style={{textAlign: 'right', paddingRight: '16px', fontWeight: 'bold'}}>ID:</Col>
         <Col  span={18}>{_.get(usage, 'id')}</Col>
         </Row> }
-        <Row style={{marginBottom: '10px'}}>
-            <Col span={2} style={{textAlign: 'right', paddingRight: '16px', fontWeight: 'bold'}}>Classification:</Col>
-        <Col  span={18}><Classification
+        {classification &&   <Row style={{marginBottom: '10px'}}>
+            <Col span={3} style={{textAlign: 'right', paddingRight: '16px', fontWeight: 'bold'}}>Classification:</Col>
+       <Col  span={18}><Classification
           classification={_.initial(classification)}
           datasetKey={_.get(usage, "name.datasetKey")}
         /></Col>
-        </Row>
+        </Row>}
      {issues &&   <Row >
-            <Col span={2} style={{textAlign: 'right', paddingRight: '16px', fontWeight: 'bold'}}>Issues:</Col>
+            <Col span={3} style={{textAlign: 'right', paddingRight: '16px', fontWeight: 'bold'}}>Issues:</Col>
         <Col  span={18}>{issues.map(i => (
             <Tag key={i} color="red">
               {i}
