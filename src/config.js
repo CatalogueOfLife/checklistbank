0dead79ab6ed249de4c1d5d5f565da520f1ef0a2
Thomas Stjernegaard <tsjeppesen@gbif.org>
2019-08-28 10:13:45 +0200

    closes #306

--- src/config.js
+++ src/config.js
@@ -1,69 +1,70 @@
 const environments = {
-    dev: {
-      url: 'https://dev.col.plus',
+  dev: {
+    url: "https://dev.col.plus",
 
-      dataApi: 'https://api-dev.col.plus/',
-      env: 'dev',
-      kibanaEnv: { 
-        name: 'col-dev-ws',
-        index: 'AWyLa2lBHCKcR6PFXu26'
-    
+    dataApi: "https://api-dev.col.plus/",
+    env: "dev",
+    kibanaEnv: {
+      name: "col-dev-ws",
+      index: "AWyLa2lBHCKcR6PFXu26"
     },
-      MANAGEMENT_CLASSIFICATION: {
-        key: 3,
-        title: "CoL draft"
-      },
-      NAME_INDEX: {
-        key: 2,
-        title: "Name Index"
-      },
-      gitBackend: "https://github.com/Sp2000/colplus-backend/tree/",
-      gitFrontend: "https://github.com/Sp2000/colplus-frontend/tree/"
+    MANAGEMENT_CLASSIFICATION: {
+      key: 3,
+      title: "CoL draft"
     },
-    prod: {
-        url: 'https://wwww.col.plus',
-        dataApi: 'https://api.col.plus/',
-        env: 'prod',
-        kibanaEnv: 'col-prod-ws',
-        MANAGEMENT_CLASSIFICATION: {
-            key: 3,
-            title: "CoL draft"
-          },
-          NAME_INDEX: {
-            key: 2,
-            title: "Name Index"
-          },
-          gitBackend: "https://github.com/Sp2000/colplus-backend/tree/",
-          gitFrontend: "https://github.com/Sp2000/colplus-frontend/tree/"
+    NAME_INDEX: {
+      key: 2,
+      title: "Name Index"
     },
-    docker: {
-        url: 'http://localhost:3000',
-        dataApi: 'http://localhost:8090/',
-        env: 'docker',
-        kibanaEnv: {name: 'col-docker',
-      index: 'AWyLa4mQHCKcR6PFXu4X'},
-        MANAGEMENT_CLASSIFICATION: {
-            key: 3,
-            title: "CoL draft"
-          },
-          NAME_INDEX: {
-            key: 2,
-            title: "Name Index"
-          },
-          gitBackend: "https://github.com/Sp2000/colplus-backend/tree/",
-          gitFrontend: "https://github.com/Sp2000/colplus-frontend/tree/"
-      }
-  };
-  
-  const domain = window.location.hostname;
+    gitBackend: "https://github.com/Sp2000/colplus-backend/tree/",
+    gitFrontend: "https://github.com/Sp2000/colplus-frontend/tree/"
+  },
+  prod: {
+    url: "https://wwww.col.plus",
+    dataApi: "https://api.col.plus/",
+    env: "prod",
+    kibanaEnv: {
+      name: "col-prod-ws",
+      index: "AWyLa4mQHCKcR6PFXu4X"
+    },
+    MANAGEMENT_CLASSIFICATION: {
+      key: 3,
+      title: "CoL draft"
+    },
+    NAME_INDEX: {
+      key: 2,
+      title: "Name Index"
+    },
+    gitBackend: "https://github.com/Sp2000/colplus-backend/tree/",
+    gitFrontend: "https://github.com/Sp2000/colplus-frontend/tree/"
+  },
+  docker: {
+    url: "http://localhost:3000",
+    dataApi: "http://localhost:8090/",
+    env: "docker",
+    kibanaEnv: { name: "col-docker", index: "AWyLa4mQHCKcR6PFXu4X" },
+    MANAGEMENT_CLASSIFICATION: {
+      key: 3,
+      title: "CoL draft"
+    },
+    NAME_INDEX: {
+      key: 2,
+      title: "Name Index"
+    },
+    gitBackend: "https://github.com/Sp2000/colplus-backend/tree/",
+    gitFrontend: "https://github.com/Sp2000/colplus-frontend/tree/"
+  }
+};
 
-let env = environments.dev;
-if (domain.endsWith('www.col.plus')) {
+const domain = window.location.hostname;
+
+let env = environments.prod;
+if (domain.endsWith("www.col.plus")) {
   env = environments.prod;
-} else if (domain.endsWith('dev.col.plus')) {
+} else if (domain.endsWith("dev.col.plus")) {
   env = environments.dev;
 } /* else if (domain.endsWith('localhost')) {
   env = environments.docker;
 } */
 
-  export default env;
+export default env;
