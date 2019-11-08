84b28f48847a57fa5ad4d9a004459e684de50dd6
Thomas Stjernegaard <tsjeppesen@gbif.org>
2019-11-08 13:13:13 +0100

    closes #85

--- src/config.js
+++ src/config.js
@@ -76,7 +76,7 @@ const environments = {
 
 const domain = window.location.hostname;
 
-let env = environments.prod;
+let env = environments.dev;
 if (domain.endsWith("www.col.plus")) {
   env = environments.prod;
 } else if (domain.endsWith("dev.col.plus")) {
