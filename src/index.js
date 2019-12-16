f0148416de2dccb74c7693e14849085562fe0d32
Thomas Stjernegaard <tsjeppesen@gbif.org>
2019-12-16 14:56:17 +0100

    testing unregister service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
+// test unregister
+unregister()
