80f2634cabe93cf21b7ce9b50b6a84a24b901eb3
Thomas Stjernegaard <tsjeppesen@gbif.org>
2020-10-06 12:15:25 +0200

    closes https://github.com/CatalogueOfLife/clearinghouse-ui/issues/728

--- src/App.js
+++ src/App.js
@@ -9,35 +9,35 @@ import DatasetPage from "./pages/DatasetKey";
 import DatasetCreate from "./pages/DatasetCreate";
 import Taxon from "./pages/Taxon";
 import Name from "./pages/Name";
-import VerbatimRecord from "./pages/VerbatimRecord"
+import VerbatimRecord from "./pages/VerbatimRecord";
 import { ThemeProvider } from "react-jss";
-import DatasetProvider from "./components/hoc/DatasetProvider"
+import DatasetProvider from "./components/hoc/DatasetProvider";
 import Assembly from "./pages/catalogue/Assembly";
 import AssemblyDuplicates from "./pages/catalogue/AssemblyDuplicates";
-import SectorSync from "./pages/catalogue/SectorSync"
-import CatalogueSectors from "./pages/catalogue/CatalogueSectors"
-import CatalogueTaxon from "./pages/catalogue/CatalogueTaxon"
-import CatalogueName from "./pages/catalogue/CatalogueName"
+import SectorSync from "./pages/catalogue/SectorSync";
+import CatalogueSectors from "./pages/catalogue/CatalogueSectors";
+import CatalogueTaxon from "./pages/catalogue/CatalogueTaxon";
+import CatalogueName from "./pages/catalogue/CatalogueName";
 import CatalogueMeta from "./pages/catalogue/CatalogueMeta";
-import CatalogueNameSearch from "./pages/catalogue/CatalogueNameSearch"
-import CatalogueDecisions from "./pages/catalogue/CatalogueDecisions"
-import CatalogueOptions from "./pages/catalogue/Options"
-import CatalogueSourceDataset from "./pages/catalogue/SourceDataset"
-import Admin from "./pages/Admin"
-import EsAdmin from "./pages/Admin/EsAdmin"
-import SectorDiff from "./pages/catalogue/SectorDiff"
+import CatalogueNameSearch from "./pages/catalogue/CatalogueNameSearch";
+import CatalogueDecisions from "./pages/catalogue/CatalogueDecisions";
+import CatalogueOptions from "./pages/catalogue/Options";
+import CatalogueSourceDataset from "./pages/catalogue/SourceDataset";
+import Admin from "./pages/Admin";
+import EsAdmin from "./pages/Admin/EsAdmin";
+import SectorDiff from "./pages/catalogue/SectorDiff";
 import Imports from "./pages/Imports";
 import ContextProvider from "./components/hoc/ContextProvider";
 import Exception404 from "./components/exception/404";
 import Helmet from "react-helmet";
 import CatalogueReferences from "./pages/catalogue/CatalogueReferences";
-import HomePage from "./pages/HomePage"
-import NameIndex from "./pages/NameIndex"
-import CatalogueSources from "./pages/catalogue/CatalogueSources"
-import CatalogueSourceMetrics from "./pages/catalogue/CatalogueSourceMetrics"
+import HomePage from "./pages/HomePage";
+import NameIndex from "./pages/NameIndex";
+import CatalogueSources from "./pages/catalogue/CatalogueSources";
+import CatalogueSourceMetrics from "./pages/catalogue/CatalogueSourceMetrics";
 
 const theme = {
-  colorPrimary: "deepskyblue"
+  colorPrimary: "deepskyblue",
 };
 
 class App extends Component {
@@ -50,206 +50,193 @@ class App extends Component {
           <link rel="canonical" href="https://data.catalogue.life/" />
         </Helmet>
         <Router history={history}>
-        <React.Fragment>
-          <ThemeProvider theme={theme}>
-            <Switch>
-              <Route
-                exact
-                key="HomePage"
-                path="/"
-                component={HomePage}
-              />
-              <Route
-                exact
-                key="NameIndex"
-                path="/names"
-                render={({ match, location }) => (
-                  <NameIndex section={match.params.section} location={location} />
-                )}
-              />
-              <PrivateRoute
-                exact
-                key="catalogueSources"
-                path="/catalogue/:catalogueKey/sources"
-                component={CatalogueSources}
-              />
-               <PrivateRoute
-                exact
-                key="catalogueSourceMetrics"
-                path="/catalogue/:catalogueKey/sourcemetrics"
-                component={CatalogueSourceMetrics}
-              />
-              <PrivateRoute
-                exact
-                key="Admin"
-                path={`/admin/settings`}
-                roles={["editor", "admin"]}
-                component={Admin}
-              />
-              <PrivateRoute
-                exact
-                key="EsAdmin"
-                path={`/admin/es`}
-                roles={["editor", "admin"]}
-                component={EsAdmin}
-              />
-              <PrivateRoute
-                exact
-                key="References"
-                path="/catalogue/:catalogueKey/references/:key?"
-                render={({ match, location }) => (
-                  <CatalogueReferences section={match.params.section} location={location} />
-                )}
-              />
-              <Route
-                exact
-                key="imports"
-                path="/imports/:section?"
-                render={({ match, location }) => (
-                  <Imports section={match.params.section} location={location} />
-                )}
-              />
-              <PrivateRoute
-                exact
-                key="CatalogueOptions"
-                path={`/catalogue/:catalogueKey/options`}
-                roles={["editor"]}
-                component={CatalogueOptions}
-              />
-              <PrivateRoute
-                exact
-                key="Assembly"
-                path={`/catalogue/:catalogueKey/assembly`}
-                roles={["editor"]}
-                component={Assembly}
-              />
-              <PrivateRoute
-                exact
-                key="AssemblyDuplicates"
-                path={`/catalogue/:catalogueKey/duplicates`}
-                roles={["editor"]}
-                component={AssemblyDuplicates}
-              />
-              <PrivateRoute
-                exact
-                key="catalogueMeta"
-                path="/catalogue/:catalogueKey/meta"
-                component={CatalogueMeta}
-                
-              />
-              <PrivateRoute
-                exact
-                key="catalogueNameSearch"
-                path="/catalogue/:catalogueKey/names"
-                component={CatalogueNameSearch}
-                
-              />
-              
-              <PrivateRoute
-                exact
-                key="sectorSync"
-                path="/catalogue/:catalogueKey/sector/sync"
-                render={({ match, location }) => (
-                  <SectorSync section={match.params.section} match={match} location={location} />
-                )}
-                
-              />
-              <PrivateRoute
-                exact
-                key="sector"
-                path="/catalogue/:catalogueKey/sector"
-                component={CatalogueSectors}
-                
-              />
-              <PrivateRoute
-                exact
-                key="decisions"
-                path="/catalogue/:catalogueKey/decision"
-                component={CatalogueDecisions}
-                
-              /> 
-              
-              <PrivateRoute
-                exact
-                key="sectorDiff"
-                path="/catalogue/:catalogueKey/sync/:sectorKey/diff"
-                component={SectorDiff}
-                
-              />
+          <React.Fragment>
+            <ThemeProvider theme={theme}>
+              <Switch>
+                <Route exact key="HomePage" path="/" component={HomePage} />
+                <Route
+                  exact
+                  key="NameIndex"
+                  path="/names"
+                  render={({ match, location }) => (
+                    <NameIndex
+                      section={match.params.section}
+                      location={location}
+                    />
+                  )}
+                />
+                <PrivateRoute
+                  exact
+                  key="catalogueSources"
+                  path="/catalogue/:catalogueKey/sources"
+                  component={CatalogueSources}
+                />
+                <PrivateRoute
+                  exact
+                  key="catalogueSourceMetrics"
+                  path="/catalogue/:catalogueKey/sourcemetrics"
+                  component={CatalogueSourceMetrics}
+                />
+                <PrivateRoute
+                  exact
+                  key="Admin"
+                  path={`/admin/settings`}
+                  roles={["editor", "admin"]}
+                  component={Admin}
+                />
+                <PrivateRoute
+                  exact
+                  key="EsAdmin"
+                  path={`/admin/es`}
+                  roles={["editor", "admin"]}
+                  component={EsAdmin}
+                />
+                <PrivateRoute
+                  exact
+                  key="References"
+                  path="/catalogue/:catalogueKey/references/:key?"
+                  render={({ match, location }) => (
+                    <CatalogueReferences
+                      section={match.params.section}
+                      location={location}
+                    />
+                  )}
+                />
+                <Route
+                  exact
+                  key="imports"
+                  path="/imports/:section?"
+                  render={({ match, location }) => (
+                    <Imports
+                      section={match.params.section}
+                      location={location}
+                    />
+                  )}
+                />
+                <PrivateRoute
+                  exact
+                  key="CatalogueOptions"
+                  path={`/catalogue/:catalogueKey/options`}
+                  roles={["editor"]}
+                  component={CatalogueOptions}
+                />
+                <PrivateRoute
+                  exact
+                  key="Assembly"
+                  path={`/catalogue/:catalogueKey/assembly`}
+                  roles={["editor"]}
+                  component={Assembly}
+                />
+                <PrivateRoute
+                  exact
+                  key="AssemblyDuplicates"
+                  path={`/catalogue/:catalogueKey/duplicates`}
+                  roles={["editor"]}
+                  component={AssemblyDuplicates}
+                />
+                <PrivateRoute
+                  exact
+                  key="catalogueMeta"
+                  path="/catalogue/:catalogueKey/meta"
+                  component={CatalogueMeta}
+                />
+                <PrivateRoute
+                  exact
+                  key="catalogueNameSearch"
+                  path="/catalogue/:catalogueKey/names"
+                  component={CatalogueNameSearch}
+                />
 
-              <PrivateRoute
-                exact
-                key="catalogueTaxon"
-                path="/catalogue/:catalogueKey/taxon/:taxonOrNameKey"
-                component={CatalogueTaxon}
-                
-              />
-              <PrivateRoute
-                exact
-                key="catalogueName"
-                path="/catalogue/:catalogueKey/name/:taxonOrNameKey"
-                component={CatalogueName}
-                
-              />
-              
-          
-              <PrivateRoute
-                exact
-                key="datasetCreate"
-                path={`/newdataset`}
-                roles={["editor", "admin"]}
-                component={DatasetCreate}
-              />
-              
-              <PrivateRoute
-                exact
-                key="CatalogueSourceDataset"
-                path={`/catalogue/:catalogueKey/dataset/:key/:section(issues|tasks|workbench|duplicates|meta|classification|references|imports|verbatim|taxon|name)/:taxonOrNameKey?`}
-                component={CatalogueSourceDataset}
-              
-              />
-              <PrivateRoute
-                exact
-                key="datasetKey"
-                path={`/catalogue/:catalogueKey/dataset/:key/:section:(imports|classification|sectors|meta|names|taxon|name|verbatim)/:taxonOrNameKey?`}
-                component={DatasetPage}
-              
-              />
-              <Route
-                exact
-                key="datasetKey2"
-                path={`/dataset/:key/:section?/:taxonOrNameKey?`}
-                component={DatasetPage}
-              
-              />
-              <Route
-                exact
-                key="dataset"
-                path="/dataset"
-                render={props => <DatasetList location={props.location} />}
-              />
-              <Route component={Exception404} />
-            </Switch>
-            
-          </ThemeProvider>
-          <Route   
-                         
-                key="datasetProvider"
-                path={`/dataset/:key`}
-                component={DatasetProvider}
-              /> 
-          <Route   
-                         
-                key="sourceDatasetProvider"
-                path={`/catalogue/:catalogueKey/dataset/:key`}
-                component={DatasetProvider}
-              /> 
-              <Route            
-                key="catalogueProvider"
-                path={`/catalogue/:catalogueKey`}
-                component={DatasetProvider}
-              /> 
-              </React.Fragment>
+                <PrivateRoute
+                  exact
+                  key="sectorSync"
+                  path="/catalogue/:catalogueKey/sector/sync"
+                  component={SectorSync}
+                />
+                <PrivateRoute
+                  exact
+                  key="sector"
+                  path="/catalogue/:catalogueKey/sector"
+                  component={CatalogueSectors}
+                />
+                <PrivateRoute
+                  exact
+                  key="decisions"
+                  path="/catalogue/:catalogueKey/decision"
+                  component={CatalogueDecisions}
+                />
+
+                <PrivateRoute
+                  exact
+                  key="sectorDiff"
+                  path="/catalogue/:catalogueKey/sync/:sectorKey/diff"
+                  component={SectorDiff}
+                />
+
+                <PrivateRoute
+                  exact
+                  key="catalogueTaxon"
+                  path="/catalogue/:catalogueKey/taxon/:taxonOrNameKey"
+                  component={CatalogueTaxon}
+                />
+                <PrivateRoute
+                  exact
+                  key="catalogueName"
+                  path="/catalogue/:catalogueKey/name/:taxonOrNameKey"
+                  component={CatalogueName}
+                />
+
+                <PrivateRoute
+                  exact
+                  key="datasetCreate"
+                  path={`/newdataset`}
+                  roles={["editor", "admin"]}
+                  component={DatasetCreate}
+                />
+
+                <PrivateRoute
+                  exact
+                  key="CatalogueSourceDataset"
+                  path={`/catalogue/:catalogueKey/dataset/:key/:section(issues|tasks|workbench|duplicates|meta|classification|references|imports|verbatim|taxon|name)/:taxonOrNameKey?`}
+                  component={CatalogueSourceDataset}
+                />
+                <PrivateRoute
+                  exact
+                  key="datasetKey"
+                  path={`/catalogue/:catalogueKey/dataset/:key/:section:(imports|classification|sectors|meta|names|taxon|name|verbatim)/:taxonOrNameKey?`}
+                  component={DatasetPage}
+                />
+                <Route
+                  exact
+                  key="datasetKey2"
+                  path={`/dataset/:key/:section?/:taxonOrNameKey?`}
+                  component={DatasetPage}
+                />
+                <Route
+                  exact
+                  key="dataset"
+                  path="/dataset"
+                  render={(props) => <DatasetList location={props.location} />}
+                />
+                <Route component={Exception404} />
+              </Switch>
+            </ThemeProvider>
+            <Route
+              key="datasetProvider"
+              path={`/dataset/:key`}
+              component={DatasetProvider}
+            />
+            <Route
+              key="sourceDatasetProvider"
+              path={`/catalogue/:catalogueKey/dataset/:key`}
+              component={DatasetProvider}
+            />
+            <Route
+              key="catalogueProvider"
+              path={`/catalogue/:catalogueKey`}
+              component={DatasetProvider}
+            />
+          </React.Fragment>
         </Router>
       </ContextProvider>
     );
--- src/pages/catalogue/SectorSync/index.js
+++ src/pages/catalogue/SectorSync/index.js
@@ -1,9 +1,8 @@
 import React from "react";
-
+import { withRouter } from "react-router-dom";
 import Layout from "../../../components/LayoutNew";
 import SyncTable from "./SyncTable";
-import withContext from "../../../components/hoc/withContext"
-
+import withContext from "../../../components/hoc/withContext";
 
 class SectorSync extends React.Component {
   constructor(props) {
@@ -11,20 +10,20 @@ class SectorSync extends React.Component {
   }
 
   render() {
-    const {catalogue} = this.props;
+    const { catalogue } = this.props;
     return (
       <Layout
         selectedKeys={["sectorSync"]}
         openKeys={["assembly", "projectDetails"]}
-        title={catalogue ? catalogue.title : ''}
+        title={catalogue ? catalogue.title : ""}
       >
-        <SyncTable location={this.props.location} match={this.props.match}/>
+        <SyncTable location={this.props.location} match={this.props.match} />
       </Layout>
     );
   }
 }
 
 const mapContextToProps = ({ catalogue }) => ({
-  catalogue
+  catalogue,
 });
-export default withContext(mapContextToProps)(SectorSync);
+export default withContext(mapContextToProps)(withRouter(SectorSync));
