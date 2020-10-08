7302b271a884a6a33552aee409b218712c81fcdc
Thomas Stjernegaard <tsjeppesen@gbif.org>
2020-10-08 13:20:49 +0200

    closes https://github.com/CatalogueOfLife/clearinghouse-ui/issues/615

--- src/pages/Taxon/Classification.js
+++ src/pages/Taxon/Classification.js
@@ -3,38 +3,72 @@ import _ from "lodash";
 import { NavLink, withRouter } from "react-router-dom";
 import PresentationItem from "../../components/PresentationItem";
 
-const getDatasetClassificationRoute = (location, datasetKey, catalogueKey) => {
-  return location.pathname.startsWith(`/catalogue/${catalogueKey}`) ?  `/catalogue/${catalogueKey}/dataset/${datasetKey}/classification`: `/dataset/${datasetKey}/classification`
-}
+const getDatasetTaxonRoute = (location, datasetKey, catalogueKey) => {
+  return location.pathname.startsWith(`/catalogue/${catalogueKey}`)
+    ? `/catalogue/${catalogueKey}/dataset/${datasetKey}/taxon/`
+    : `/dataset/${datasetKey}/taxon/`;
+};
 
 const isAssembly = (location, catalogueKey) => {
-  return location.pathname.startsWith(`/catalogue/${catalogueKey}`) && location.pathname.indexOf('/dataset') === -1
-}
+  return (
+    location.pathname.startsWith(`/catalogue/${catalogueKey}`) &&
+    location.pathname.indexOf("/dataset") === -1
+  );
+};
 
-const ClassificationTable = ({ datasetKey, data, taxon, style, catalogueKey, location }) => (
-  <div style={style}> {_.reverse([...data]).map(t => (
-    <PresentationItem md={6} label={_.startCase(t.name.rank)} classes={{formItem: {borderBottom: 'none'}}} key={t.name.rank}>
+const ClassificationTable = ({
+  datasetKey,
+  data,
+  taxon,
+  style,
+  catalogueKey,
+  location,
+}) => (
+  <div style={style}>
+    {" "}
+    {_.reverse([...data]).map((t) => (
+      <PresentationItem
+        md={6}
+        label={_.startCase(t.name.rank)}
+        classes={{ formItem: { borderBottom: "none" } }}
+        key={t.name.rank}
+      >
+        <NavLink
+          to={{
+            pathname: isAssembly(location, catalogueKey)
+              ? `/catalogue/${catalogueKey}/taxon/${_.get(t, "id")}`
+              : `${getDatasetTaxonRoute(
+                  location,
+                  datasetKey,
+                  catalogueKey
+                )}${_.get(t, "id")}`,
+          }}
+        >
+          <span dangerouslySetInnerHTML={{ __html: t.labelHtml }} />
+        </NavLink>
+      </PresentationItem>
+    ))}
+    <PresentationItem
+      md={6}
+      label={_.get(taxon, "name.rank") ? _.startCase(taxon.name.rank) : ""}
+      classes={{ formItem: { borderBottom: "none" } }}
+    >
       <NavLink
         to={{
-          pathname: isAssembly(location, catalogueKey) ? `/catalogue/${catalogueKey}/assembly` : getDatasetClassificationRoute(location, datasetKey, catalogueKey ),
-          search: `?${isAssembly(location, catalogueKey)  ? 'assemblyTaxonKey' : 'taxonKey'}=${t.id}`
+          pathname: isAssembly(location, catalogueKey)
+            ? `/catalogue/${catalogueKey}/taxon/${_.get(taxon, "id")}`
+            : `${getDatasetTaxonRoute(
+                location,
+                datasetKey,
+                catalogueKey
+              )}${_.get(taxon, "id")}`,
         }}
       >
-        <span dangerouslySetInnerHTML={{ __html: t.labelHtml }} />
+        {_.get(taxon, "labelHtml") && (
+          <span dangerouslySetInnerHTML={{ __html: taxon.labelHtml }} />
+        )}
       </NavLink>
     </PresentationItem>
-  ))} 
-  <PresentationItem md={6} label={_.get(taxon, 'name.rank') ? _.startCase(taxon.name.rank) : ''} classes={{formItem: {borderBottom: 'none'}}} >
-      <NavLink
-        to={{
-          pathname: isAssembly(location, catalogueKey) ? `/catalogue/${catalogueKey}/assembly` : getDatasetClassificationRoute(location, datasetKey, catalogueKey ),
-          search: `?${isAssembly(location, catalogueKey)  ? 'assemblyTaxonKey' : 'taxonKey'}=${_.get(taxon, 'id')}`
-        }}
-      >
-      { _.get(taxon, 'labelHtml') && <span dangerouslySetInnerHTML={{ __html: taxon.labelHtml }} />}
-      </NavLink>
-    </PresentationItem>
-        
   </div>
 );
 
