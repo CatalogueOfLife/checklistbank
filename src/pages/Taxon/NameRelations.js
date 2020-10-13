ab2159f2c44dfbd266ed982775cd83dd7371616f
Thomas Stjernegaard <tsjeppesen@gbif.org>
2020-10-13 12:55:08 +0200

    closes https://github.com/CatalogueOfLife/clearinghouse-ui/issues/748

--- src/pages/Taxon/NameRelations.js
+++ src/pages/Taxon/NameRelations.js
@@ -4,33 +4,42 @@ import { NavLink } from "react-router-dom";
 import PresentationItem from "../../components/PresentationItem";
 
 const typeMap = {
-    "spelling correction" : "of",
-    "based on" : "",
-    "replacement name": "for",
-    "later homonym": "of",
-    "superfluous": "name for"
-}
+  "spelling correction": "of",
+  "based on": "",
+  "replacement name": "for",
+  "later homonym": "of",
+  superfluous: "name for",
+};
 
 const NameRelations = ({ data, style, catalogueKey, datasetKey }) => {
   return (
     <div style={style}>
-      {data
-        .map(r => (
-          <PresentationItem key={r.key} label={`${_.capitalize(r.type)} ${typeMap[r.type] ? typeMap[r.type]: ""}` } helpText={r.note}>
-           <NavLink
-              to={{
-                pathname: datasetKey === catalogueKey ? `/catalogue/${catalogueKey}/name/${encodeURIComponent(
-                  r.relatedName.id
-                )}` : `/dataset/${datasetKey}/name/${encodeURIComponent(
-                  r.relatedName.id
-                )}`
-              }}
-              exact={true}
-            >
-            <span dangerouslySetInnerHTML={{__html: r.relatedName.labelHtml}}></span>
-            </NavLink>
-          </PresentationItem>
-        ))}
+      {data.map((r) => (
+        <PresentationItem
+          key={r.key}
+          label={`${_.capitalize(r.type)} ${
+            typeMap[r.type] ? typeMap[r.type] : ""
+          }`}
+          helpText={r.note}
+        >
+          <NavLink
+            to={{
+              pathname:
+                datasetKey === catalogueKey
+                  ? `/catalogue/${catalogueKey}/name/${encodeURIComponent(
+                      r.relatedName.id
+                    )}`
+                  : `/dataset/${datasetKey}/name/${encodeURIComponent(
+                      r.relatedName.id
+                    )}`,
+            }}
+            exact={true}
+          >
+            {/*<span dangerouslySetInnerHTML={{ __html: r.relatedName.labelHtml }}></span> */}
+            {`${r.relatedName.scientificName} ${r.relatedName.authorship}`}
+          </NavLink>
+        </PresentationItem>
+      ))}
     </div>
   );
 };
