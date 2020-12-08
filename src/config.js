2c597294a442da240b7140fc90171388f6567284
Markus <mdoering@gbif.org>
2020-12-08 11:28:24 +0100

    Allow both domains for prod

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import environments from "./env.json";
 const domain = window.location.hostname;
 
 let env = environments.dev;
-if (domain.endsWith("data.catalogueoflife.org")) {
+if (domain.endsWith("data.catalogueoflife.org") || domain.endsWith("data.catalogue.life")) {
   env = environments.prod;
 } else if (domain.endsWith("localhost")) {
   env = environments.dev;
