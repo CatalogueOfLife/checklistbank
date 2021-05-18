ea1bdea73fcc099029888478d9ec47db4fdf3f73
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-05-18 13:16:23 +0200

    closes https://github.com/CatalogueOfLife/checklistbank/issues/869

--- src/components/PersonPresentation.js
+++ src/components/PersonPresentation.js
@@ -1,9 +1,27 @@
 import React from "react";
 
-
-export default ({person, style}) => 
-<span style={style} >
-    <span style={{display: 'block'}}>{`${person.familyName}${person.givenName ? ', '+person.givenName : ''}`}</span>
-      {person.email && <a style={{display: 'block'}} href={`mailto:${person.email}`}>{person.email}</a>}
-      {person.orcid && <a style={{display: 'block'}} href={`https://orcid.org/${person.orcid}`}><img src="/images/orcid_16x16.png" style={{ flex: '0 0 auto' }} alt=""></img> {person.orcid}</a>}
-</span>
+export default ({ person, style }) => (
+  <span style={style}>
+    <span style={{ display: "block" }}>{`${person.familyName || "-"}${
+      person.givenName ? ", " + person.givenName : ""
+    }`}</span>
+    {person.email && (
+      <a style={{ display: "block" }} href={`mailto:${person.email}`}>
+        {person.email}
+      </a>
+    )}
+    {person.orcid && (
+      <a
+        style={{ display: "block" }}
+        href={`https://orcid.org/${person.orcid}`}
+      >
+        <img
+          src="/images/orcid_16x16.png"
+          style={{ flex: "0 0 auto" }}
+          alt=""
+        ></img>{" "}
+        {person.orcid}
+      </a>
+    )}
+  </span>
+);
