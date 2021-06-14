8c257f87cbfae0d0adfe622e57cabeecdb42c673
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-06-14 09:53:08 +0200

    Render CSL preview when adding source

--- src/components/MetaData/CitationPresentation.js
+++ src/components/MetaData/CitationPresentation.js
@@ -1,5 +1,25 @@
 import React from "react";
+import locales from "../csl/locales";
+import styles from "../csl/styles";
+const CSL = require("citeproc");
+const processCsl = (source, styleKey = "zookeys") => {
+  const references = { [source.id || "id"]: { id: "id", ...source } };
+  const citeprocSys = {
+    retrieveLocale: function (lang) {
+      return locales[lang];
+    },
+    retrieveItem: function (id) {
+      return references[id]; //{ ...references[id].csl, id: id };
+    },
+  };
 
-const CitationPresentation = ({ csl }) => (csl ? csl.title : "");
+  const citeproc = new CSL.Engine(citeprocSys, styles[styleKey]);
+  citeproc.updateItems(Object.keys(references));
+  const bibResult = citeproc.makeBibliography();
+  return bibResult[1];
+  //return Promise.resolve(bibResult[1].join("\n"));
+};
+const CitationPresentation = ({ csl }) =>
+  csl ? <div dangerouslySetInnerHTML={{ __html: processCsl(csl) }}></div> : "";
 
 export default CitationPresentation;
