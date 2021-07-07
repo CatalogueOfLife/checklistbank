379a46ff7c884e1306125d8aed0231d4db6bdcd4
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-07-07 10:41:59 +0200

    closes https://github.com/CatalogueOfLife/checklistbank/issues/918

--- src/pages/DatasetImportMetrics/ImportHistory.js
+++ src/pages/DatasetImportMetrics/ImportHistory.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ArrowRightOutlined, CodeOutlined, LoadingOutlined } from '@ant-design/icons';
+import { ArrowRightOutlined, CodeOutlined, LoadingOutlined, DiffOutlined } from '@ant-design/icons';
 import { Timeline, Tooltip } from "antd";
 import moment from "moment";
 import { NavLink } from "react-router-dom";
@@ -28,9 +28,14 @@ const getDot = (h, attempt)=>{
   
 }
 
+const getPreviousFinishedImport = (importHistory, index) => {
+  const prevHistory = importHistory.slice(index +1);
+  return _.get(prevHistory.filter(h => h.state === "finished"), '[0].attempt')
+}
+
 const ImportHistory = ({ importHistory, attempt, catalogueKey }) => (
   <Timeline>
-    {importHistory.map(h => (
+    {importHistory.map((h, index) => (
       <Timeline.Item key={h.attempt} color={tagColors[h.state]} dot={getDot(h, attempt)}>
         {['finished', 'failed', 'unchanged'].indexOf(h.state) === -1 && <strong>{h.state}</strong>}
         {(h.state === "finished" || h.state === "unchanged") && (
@@ -44,7 +49,21 @@ const ImportHistory = ({ importHistory, attempt, catalogueKey }) => (
           <strong>{`${h.state}`} {'upload' in h && (_.get(h, 'upload') ? <UploadOutlined/> : <DownloadOutlined/>)}</strong>
         </NavLink>
           {_.get(h, 'user.username') && <p>{_.get(h, 'upload') ? "Upload" : "Download"} by {h.user.username}</p>}
-            <p>{`${moment(h.started).format("lll")}`} {" "} <Tooltip title="Kibana logs" placement="right"><a href={kibanaQuery(h.datasetKey, h.attempt)} target="_blank" ><CodeOutlined /></a></Tooltip></p>
+            <p><span style={{fontSize: "10px"}}>{`${moment(h.started).format("lll")}`} {" "}</span> <Tooltip title="Kibana logs" placement="right"><a href={kibanaQuery(h.datasetKey, h.attempt)} target="_blank" ><CodeOutlined /></a></Tooltip>
+            {h.state === "finished" && getPreviousFinishedImport(importHistory, index) &&  <React.Fragment> {" "}
+            <Tooltip title="Diff between this and previous attempt" placement="right">
+            <NavLink
+          to={{
+            pathname: `/dataset/${h.datasetKey}/diff`,
+            search: `?attempts=${getPreviousFinishedImport(importHistory, index)}..${h.attempt}`
+          }}
+          exact={true}
+        >
+          <DiffOutlined />
+          </NavLink>
+          </Tooltip>
+          </React.Fragment>}
+            </p>
           </React.Fragment>
         )}
         {h.state === "failed" && (
@@ -58,8 +77,9 @@ const ImportHistory = ({ importHistory, attempt, catalogueKey }) => (
             <strong>{`${h.state}`}</strong>
             </NavLink>
             {" "}  <Tooltip title="Kibana logs" placement="right"><a href={kibanaQuery(h.datasetKey, h.attempt)} target="_blank" ><CodeOutlined /></a></Tooltip>
+         
             {_.get(h, 'user.username') && <p>Created by {h.user.username}</p>}
-              <p>{`${moment(h.started).format("lll")}`}</p>
+              <p style={{fontSize: "10px"}}>{`${moment(h.started).format("lll")}`}</p>
               <p>{h.error.length > 200 ? `${h.error.substring(0, 200)} .....` : h.error}</p>
           </React.Fragment>
         )}
