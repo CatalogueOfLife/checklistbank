7c792ad3fcbf61c9db2afa00554ec4502fe5e554
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-08-12 13:06:47 +0200

    remove BibTex logic in favour of .bib extension

--- src/components/MetaData/BibTex.js
+++ src/components/MetaData/BibTex.js
@@ -1,29 +1,17 @@
 import React from "react";
-import axios from "axios";
 import config from "../../config";
 
-const BibTex = ({ datasetKey }) => {
-  const saveFile = async (blob) => {
-    const a = document.createElement("a");
-    a.download = "citation.bib";
-    a.href = URL.createObjectURL(blob);
-    a.addEventListener("click", () => {
-      setTimeout(() => URL.revokeObjectURL(a.href), 30 * 1000);
-    });
-    a.click();
+const BibTex = ({ datasetKey, style = {} }) => {
+  const defaultStyle = {
+    height: "40px",
   };
-  const getBibTex = async () => {
-    const res = await axios.get(`${config.dataApi}dataset/${datasetKey}`, {
-      responseType: "blob",
-      headers: {
-        Accept: "application/x-bibtex",
-      },
-    });
-    saveFile(res.data);
-  };
+  const url = `${config.dataApi}dataset/${datasetKey}.bib`;
   return (
-    <a onClick={getBibTex}>
-      <img src="/images/bibtex_logo.png" height="40px" />
+    <a href={url}>
+      <img
+        src="/images/bibtex_logo.png"
+        style={{ ...defaultStyle, ...style }}
+      />
     </a>
   );
 };
