e38e83bc602d8d87ca6a69daf8844aac94883237
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-11-25 15:58:08 +0100

    closes https://github.com/CatalogueOfLife/checklistbank/issues/901 bad regex

--- src/components/VerbatimPresentation.js
+++ src/components/VerbatimPresentation.js
@@ -19,9 +19,10 @@ const md = 5;
 ]; */
 const isValidURL = (string) => {
   if (typeof string !== "string") return false;
-  var res = string.match(
-    /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
-  );
+  const exp =
+    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
+  // const exp = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+  var res = string.match(exp);
   if (res == null) return false;
   else return true;
 };
