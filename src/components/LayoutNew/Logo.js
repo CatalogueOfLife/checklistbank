47850cdc78613e881426c88ff0f246ab59594009
Thomas Stjernegaard <tsjeppesen@gbif.org>
2022-02-28 15:58:43 +0100

    testing blue logo

--- src/components/LayoutNew/Logo.js
+++ src/components/LayoutNew/Logo.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Logo() {
-      return (<img src="/images/clb-yellow-64.png" style={{ height: '50px', flex: '0 0 auto' }} alt=""></img>);
+      /* return (<div
+            style={{ backgroundImage: `url("../images/clb-blue.svg")` , height: '58px', width: '58px', flex: '0 0 auto' }}
+            className='headerLogo'
+          />) */
+      return (<img src="/images/clb-blue.svg" style={{ height: '50px', flex: '0 0 auto' }} alt=""></img>);
 }
 
 export default Logo;
