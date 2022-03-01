18014e85717e3b7ba67d5b4b584f23197e8cf93b
Thomas Stjernegaard <tsjeppesen@gbif.org>
2022-03-01 12:05:30 +0100

    A bit more space around logo, grey test env message

--- src/components/LayoutNew/Logo.js
+++ src/components/LayoutNew/Logo.js
@@ -5,7 +5,7 @@ function Logo() {
             style={{ backgroundImage: `url("../images/clb-blue.svg")` , height: '58px', width: '58px', flex: '0 0 auto' }}
             className='headerLogo'
           />) */
-      return (<img src="/images/clb-blue.svg" style={{ height: '50px', flex: '0 0 auto' }} alt=""></img>);
+      return (<img src="/images/clb-blue.svg" style={{ height: '48px', flex: '0 0 auto' }} alt=""></img>);
 }
 
 export default Logo;
