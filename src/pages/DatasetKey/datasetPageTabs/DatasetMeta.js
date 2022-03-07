cfb073ab77e2b982d382adbadf38450ee6ebfc62
Thomas Stjernegaard <tsjeppesen@gbif.org>
2022-03-07 09:59:09 +0100

    closes https://github.com/CatalogueOfLife/checklistbank/issues/922

--- src/pages/DatasetKey/datasetPageTabs/DatasetMeta.js
+++ src/pages/DatasetKey/datasetPageTabs/DatasetMeta.js
@@ -87,7 +87,7 @@ class DatasetMeta extends React.Component {
   getSourceMeta = () => {
     const { id, catalogueKey, addError } = this.props;
 
-    axios(`${config.dataApi}dataset/${catalogueKey}/source/${id}`)
+    axios(`${config.dataApi}dataset/${catalogueKey}/source/${id}?original=true`)
       .then((res) => this.setState({ sourceMeta: res.data, sourceError: null }))
       .catch((err) => {
         addError(err);
