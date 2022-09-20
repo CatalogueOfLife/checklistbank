d426f6370f48474f1f2cc50117c04ee704c20f50
Thomas Stjernegaard <tsjeppesen@gbif.org>
2022-09-20 14:28:34 +0200

    closes https://github.com/CatalogueOfLife/checklistbank/issues/1107

--- src/pages/Taxon/TypeMaterial.js
+++ src/pages/Taxon/TypeMaterial.js
@@ -3,7 +3,7 @@ import config from "../../config";
 import _ from "lodash";
 import BorderedListItem from "./BorderedListItem"
 import linkify from 'linkify-html';
-import {Tag, Space} from "antd"
+import {Tag, Space, Tooltip} from "antd"
 import withContext from "../../components/hoc/withContext";
 import {IconContext} from "react-icons"
 import { GiDna1 } from "react-icons/gi";
@@ -26,22 +26,21 @@ export const getTypeColor = (status) => {
 const getLinks = (dataset, s) => {
   const gbifOccLink = dataset?.gbifKey && dataset?.gbifPublisherKey === config?.plaziGbifPublisherKey ? `https://www.gbif.org/occurrence/${dataset?.gbifKey}/${s.id}` : '';
   const ncbiLink = s?.associatedSequences && s?.associatedSequences.indexOf('ncbi.') > -1 ? s?.associatedSequences : '';
-  return (gbifOccLink || ncbiLink) && <p>
-    <br/>
-    Links: <Space>{gbifOccLink && <a  href={gbifOccLink}>GBIF <IconContext.Provider value={{ color: "green"}}><GbifLogoIcon /></IconContext.Provider></a>}{ncbiLink && <a href={ncbiLink}>GenBank <IconContext.Provider value={{ color: "black", size: "16"}}><GiDna1 /></IconContext.Provider></a>}</Space>
+  return (gbifOccLink || ncbiLink) && <span>
+    <Space>{gbifOccLink && <Tooltip title="Occurrence in GBIF"><a  href={gbifOccLink}><IconContext.Provider value={{ color: "green"}}><GbifLogoIcon /></IconContext.Provider></a></Tooltip>}{ncbiLink && <Tooltip title="DNA sequence in GenBank"><a href={ncbiLink}>GenBank <IconContext.Provider value={{ color: "black", size: "16"}}><GiDna1 /></IconContext.Provider></a></Tooltip>}</Space>
+    </span>
 
     
-    </p>
 }
 
 const TypeMaterial = ({dataset, data, nameID, style }) => {
   return (
     data[nameID] ? <div style={style}>
       {data[nameID].map(s => <BorderedListItem key={s.id}>
-            <Tag color={getTypeColor(s?.status)}>{s?.status}</Tag> {s?.citation && <span
+            <Tag color={getTypeColor(s?.status)}>{s?.status}</Tag>{getLinks(dataset,s)} {s?.citation && <span
                   dangerouslySetInnerHTML={{ __html: linkify(s?.citation)}}
                 ></span>}
-                {getLinks(dataset,s)}
+                
           </BorderedListItem>)
         }
     </div> : null
