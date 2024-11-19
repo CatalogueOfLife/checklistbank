973eb529f01490381569890c0e3c054282fde07a
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2024-11-19 13:00:16 +0100

    remove outcommented test data

--- src/pages/Vocabulary/TaxGroupTree.js
+++ src/pages/Vocabulary/TaxGroupTree.js
@@ -16,50 +16,7 @@ import {
 const { Title } = Typography;
 
 const TaxGroupTree = () => {
-  const [treeData, setData] = useState(
-    [] /* [
-    {
-      title: "parent 1",
-      key: "0-0",
-      children: [
-        {
-          title: "parent 1-0",
-          key: "0-0-0",
-          disabled: true,
-          children: [
-            {
-              title: "leaf",
-              key: "0-0-0-0",
-              disableCheckbox: true,
-            },
-            {
-              title: "leaf",
-              key: "0-0-0-1",
-            },
-          ],
-        },
-        {
-          title: "parent 1-1",
-          key: "0-0-1",
-          children: [
-            {
-              title: (
-                <span
-                  style={{
-                    color: "#1890ff",
-                  }}
-                >
-                  sss
-                </span>
-              ),
-              key: "0-0-1-0",
-            },
-          ],
-        },
-      ],
-    },
-  ] */
-  );
+  const [treeData, setData] = useState([]);
 
   useEffect(() => {
     fetch(`${config.dataApi}vocab/taxgroup`)
