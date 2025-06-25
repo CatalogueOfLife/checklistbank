a704a7f26393ce01b4bd93de87eceef574cbd304
Thomas Stjernegaard Jeppesen <tsjeppesen@gbif.org>
2025-06-25 09:43:28 +0100

    closes https://github.com/CatalogueOfLife/checklistbank/issues/1566

--- src/pages/Taxon/TaxonMedia.js
+++ src/pages/Taxon/TaxonMedia.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Image, Row, Col, Button } from "antd";
 import _ from "lodash";
 
@@ -7,37 +7,56 @@ export default ({ media }) => {
   if (!_.isArray(media)) {
     return null;
   }
-  const [limit, setLimit] = useState(PAGE_SIZE)
+  const [limit, setLimit] = useState(PAGE_SIZE);
   return (
     <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-      {media.slice(0, limit)
-        .filter((m) => m.type === "image")
+      {media
+        .slice(0, limit)
+        .filter((m) => m.type === "image" && !!m.url)
         .map((i) => (
           <Col key={i.url} span={12} style={{ paddingBottom: "12px" }}>
-           {i.url.indexOf('zenodo.org') === -1 ? <Image
-              preview={{
-                src: i.url,
-              }}
-              src={`//api.gbif.org/v1/image/unsafe/x260/${i.url}`}
-            /> : <Image
-            preview={{
-              src: i.url,
-            }}
-            height={260}
-            src={i.url}
-          /> }
+            {i.url.indexOf("zenodo.org") === -1 ? (
+              <Image
+                preview={{
+                  src: i.url,
+                }}
+                src={`//api.gbif.org/v1/image/unsafe/x260/${i.url}`}
+              />
+            ) : (
+              <Image
+                preview={{
+                  src: i.url,
+                }}
+                height={260}
+                src={i.url}
+              />
+            )}
             <div style={{ marginTop: "-4px" }}>
-              {i.title || ''}
+              {i.title || ""}
               {i.capturedBy && `©  ${i.capturedBy}`}
               {i.rightsHolder && `©  ${i.rightsHolder}`}
-              {i.link && <><br /><a href={i.link}>{i.link} </a></>}
+              {i.link && (
+                <>
+                  <br />
+                  <a href={i.link}>{i.link} </a>
+                </>
+              )}
             </div>
           </Col>
         ))}
-        {media.length > limit && <Col span={24} style={{textAlign: 'right', marginBottom: '8px'}}>
-          {limit > PAGE_SIZE && <Button style={{marginRight: "8px"}} onClick={() => setLimit(PAGE_SIZE)}>Show fewer</Button>}
-          <Button onClick={() => setLimit(limit+PAGE_SIZE)}>Show more</Button>
-          </Col>}
+      {media.length > limit && (
+        <Col span={24} style={{ textAlign: "right", marginBottom: "8px" }}>
+          {limit > PAGE_SIZE && (
+            <Button
+              style={{ marginRight: "8px" }}
+              onClick={() => setLimit(PAGE_SIZE)}
+            >
+              Show fewer
+            </Button>
+          )}
+          <Button onClick={() => setLimit(limit + PAGE_SIZE)}>Show more</Button>
+        </Col>
+      )}
     </Row>
   );
 };
